1f16a151856196912d2d135a6607f2201d6bc47e
benjaminMV99 <be.munoz.vergara99@gmail.com>
2021-01-08 23:55:34 -0300

    arreglo de conexion

--- models/Conexion.php
+++ models/Conexion.php
@@ -8,9 +8,13 @@ class Conexion
   //public static $pass = "3YYNo72sWCQ4QMDtTdXL";
   //public static $URL = "mysql:host=bgmykwwo2imhrgeas5hm-mysql.services.clever-cloud.com;dbname=bgmykwwo2imhrgeas5hm";
 
-  public static $user = "u5jpiuqu2grudqnq";
-  public static $pass = "WTlh3eHFtFfSjVHmiXHD";
-  public static $URL = "mysql:host=bm8zus3nc1qisz2xfkkb-mysql.services.clever-cloud.com;dbname=bm8zus3nc1qisz2xfkkb";
+  public static $user = "root";
+  public static $pass = "";
+  public static $URL = "mysql:host=localhost;dbname=optica_2020";
+
+  //public static $user = "u5jpiuqu2grudqnq";
+  //public static $pass = "WTlh3eHFtFfSjVHmiXHD";
+  //public static $URL = "mysql:host=bm8zus3nc1qisz2xfkkb-mysql.services.clever-cloud.com;dbname=bm8zus3nc1qisz2xfkkb";
 
   public static function conector()
   {
